9d8903a43e521836040ac9b58a9e9b68862b124e
BryanYunche <estudanteporesporte@gmail.com>
2025-07-04 14:57:31 -0300

    Oito rainhas feita

--- OitoRainhas.py
+++ OitoRainhas.py
@@ -1,14 +1,36 @@
-from random import randint
-
-
 class OitoRainhas:
 
     def __initi__(self):
         self.tabuleiro = []
+        self.estadosFilho = []
         self.colisao = 0
 
     def geraTabuleiro(self):
         estado = []
         for i in range(8):
-            estado.append(randint(0, 7))
-        return estado
+            estado.append(0)
+            self.tabuleiro = estado
+        self.calculaColisoes()
+        return self.tabuleiro
+
+    def calculaColisoes(self):
+        colisoes = 0
+        for i in range(len(self.tabuleiro)):
+            for j in range(i + 1, len(self.tabuleiro)):
+                if self.tabuleiro[i] == self.tabuleiro[j]:
+                    colisoes += 1
+                elif abs(self.tabuleiro[i] - self.tabuleiro[j]) == abs(i - j):
+                    colisoes += 1
+        self.colisao = colisoes
+
+    def getColisoes(self):
+        return self.colisao
+
+    def mudaPosicao(self, coluna, linha):
+        if (linha and coluna) in range(8):
+            self.tabuleiro[coluna] = linha
+            self.calculaColisoes()
+            return self.tabuleiro
+        else:
+            print("Digite um valor entre 0 e 7")
+
--- main.py
+++ main.py
@@ -3,5 +3,13 @@ from OitoRainhas import OitoRainhas
 tabuleiro = OitoRainhas()
 
 estado01 = tabuleiro.geraTabuleiro()
+colisao = tabuleiro.getColisoes()
 
 print(estado01)
+print(colisao)
+
+estado01 = tabuleiro.mudaPosicao(0, 1)
+colisao = tabuleiro.getColisoes()
+
+print(estado01)
+print(colisao)
